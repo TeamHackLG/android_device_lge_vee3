8c9fcf00911b14bfcaa9cdbf22ad90d932cca91f
Caio99BR <caiooliveirafarias0@gmail.com>
2015-01-01 11:15:38 -0200

    E435: Happy new year!
    Signed-off-by: Caio99BR <caiooliveirafarias0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -100,6 +100,7 @@ BOARD_QCOM_VOIP_ENABLED := true
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO := true
 TARGET_HAS_QACT := true
+BOARD_NO_SPEAKER := true
 
 # Display
 TARGET_QCOM_DISPLAY_VARIANT := legacy
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Project vee3!
 LG Optimus L3 II Project
 
 
-Copyright 2014 - The CyanogenMod Project
+Copyright 2015 - The CyanogenMod Project
