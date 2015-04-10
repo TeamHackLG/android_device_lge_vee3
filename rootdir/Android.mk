3543a96815c41ba215042efe8ccf1ac973506607
Caio99BR <caiooliveirafarias0@gmail.com>
2015-04-09 23:52:16 -0300

    Is more faster but not work if not copied

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,10 +1,18 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-# Vee3 Fstab
+# File System Table
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.vee3
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
