e699ebd4cd8aa4912313c4ad6fc7f129ccf8e1ae
Caio99BR <caiooliveirafarias0@gmail.com>
2015-05-16 20:59:32 -0300

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -1,10 +1,8 @@
 # For userdebug builds
 ifeq ($(TARGET_BUILD_VARIANT),userdebug)
-ADDITIONAL_DEFAULT_PROPERTIES += \
-	ro.secure=false \
-	ro.adb.secure=false
+# Enable ADB by default
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=false ro.adb.secure=false
 endif
-
 # For Single and Dual Sim devices
 ifeq ($(PROJECT_VEE3_SINGLE),true)
 # Single Sim
@@ -14,60 +12,41 @@ else
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
 endif
 # USB
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp \
-
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
 # QCOM
-PRODUCT_PROPERTY_OVERRIDES += \
-	com.qc.hardware=true \
-	ro.vendor.extension_library=/system/lib/libqc-opt.so \
-
+PRODUCT_PROPERTY_OVERRIDES += com.qc.hardware=true
+# Vendor Lib
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.extension_library=/system/lib/libqc-opt.so
 # OpenGL
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.opengles.version=131072 \
-
+PRODUCT_PROPERTY_OVERRIDES += ro.opengles.version=131072
 # WiFi
-PRODUCT_PROPERTY_OVERRIDES += \
-	wifi.interface=wlan0 \
-
+PRODUCT_PROPERTY_OVERRIDES += wifi.interface=wlan0
 # KSM
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.ksm.default=true \
-
+PRODUCT_PROPERTY_OVERRIDES += ro.ksm.default=true
 # Lcd Density
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=120 \
-
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=120
 # Enforce Phone Mode view
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.screen.layout=normal \
-
+PRODUCT_PROPERTY_OVERRIDES += ro.screen.layout=normal
 # Display Composition
-PRODUCT_PROPERTY_OVERRIDES += \
-	debug.sf.hw=true \
-	debug.composition.type=dyn \
-	persist.hwc.mdpcomp.enable=false \
-
+PRODUCT_PROPERTY_OVERRIDES += debug.composition.type=dyn
+PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable=false
+# Use Hardware Rendering
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.hw=true
 # Media StageFright
-PRODUCT_PROPERTY_OVERRIDES += \
-	media.stagefright.enable-player=true \
-	media.stagefright.enable-meta=false \
-	media.stagefright.enable-scan=true \
-	media.stagefright.enable-http=true \
-	media.stagefright.enable-fma2dp=true \
-	media.stagefright.enable-aac=true \
-	media.stagefright.enable-qcp=true \
-
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-player=true
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-meta=false
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-scan=true
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-http=true
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-fma2dp=true
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-aac=true
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-qcp=true
 # RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-	rild.libpath=/system/lib/libril-qc-qmi-1.so \
-	rild.libargs=-d /dev/smd0 \
-	ril.subscription.types=NV,RUIM \
-	ro.telephony.ril.v3=skippinpukcount,qcomdsds,signalstrength \
-	ro.telephony.call_ring.multiple=false \
-	ro.telephony.call_ring=false \
-
+PRODUCT_PROPERTY_OVERRIDES += rild.libpath=/system/lib/libril-qc-qmi-1.so
+PRODUCT_PROPERTY_OVERRIDES += rild.libargs=-d /dev/smd0
+PRODUCT_PROPERTY_OVERRIDES += ril.subscription.types=NV,RUIM
+PRODUCT_PROPERTY_OVERRIDES += ro.telephony.ril.v3=skippinpukcount,qcomdsds,signalstrength
+PRODUCT_PROPERTY_OVERRIDES += ro.telephony.call_ring.multiple=false
+PRODUCT_PROPERTY_OVERRIDES += ro.telephony.call_ring=false
 # Debug
-PRODUCT_PROPERTY_OVERRIDES += \
-	debug.gralloc.map_fb_memory=true \
-	debug.hwc.fakevsync=true \
+PRODUCT_PROPERTY_OVERRIDES += debug.gralloc.map_fb_memory=true
+PRODUCT_PROPERTY_OVERRIDES += debug.hwc.fakevsync=true
