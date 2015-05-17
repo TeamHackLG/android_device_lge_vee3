1c185fc75f1f35d54e3616f07222cafb0fa26a81
Caio99BR <caiooliveirafarias0@gmail.com>
2015-05-17 18:52:50 -0300

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -1,45 +1,46 @@
 # For userdebug builds
 ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 # Enable ADB by default
-ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=false ro.adb.secure=false
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure="false"
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure="false"
 endif
 # For Single and Dual Sim devices
 ifeq ($(PROJECT_VEE3_SINGLE),true)
 # Single Sim
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.radio.multisim.config=none
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.radio.multisim.config="none"
 else
 # Dual Sim
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.radio.multisim.config="dsds"
 endif
 # USB
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config="mtp"
 # QCOM
-PRODUCT_PROPERTY_OVERRIDES += com.qc.hardware=true
+PRODUCT_PROPERTY_OVERRIDES += com.qc.hardware="true"
 # Vendor Lib
-PRODUCT_PROPERTY_OVERRIDES += ro.vendor.extension_library=/system/lib/libqc-opt.so
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.extension_library="/system/lib/libqc-opt.so"
 # OpenGL
-PRODUCT_PROPERTY_OVERRIDES += ro.opengles.version=131072
+PRODUCT_PROPERTY_OVERRIDES += ro.opengles.version="131072"
 # WiFi
-PRODUCT_PROPERTY_OVERRIDES += wifi.interface=wlan0
+PRODUCT_PROPERTY_OVERRIDES += wifi.interface="wlan0"
 # KSM
-PRODUCT_PROPERTY_OVERRIDES += ro.ksm.default=true
+PRODUCT_PROPERTY_OVERRIDES += ro.ksm.default="true"
 # Lcd Density
-PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=120
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density="120"
 # Enforce Phone Mode view
-PRODUCT_PROPERTY_OVERRIDES += ro.screen.layout=normal
+PRODUCT_PROPERTY_OVERRIDES += ro.screen.layout="normal"
 # Display Composition
-PRODUCT_PROPERTY_OVERRIDES += debug.composition.type=dyn
-PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable=false
+PRODUCT_PROPERTY_OVERRIDES += debug.composition.type="dyn"
+PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable="false"
 # Use Hardware Rendering
-PRODUCT_PROPERTY_OVERRIDES += debug.sf.hw=true
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.hw="true"
 # Media StageFright
-PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-player=true
-PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-meta=false
-PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-scan=true
-PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-http=true
-PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-fma2dp=true
-PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-aac=true
-PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-qcp=true
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-player="true"
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-meta="false"
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-scan="true"
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-http="true"
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-fma2dp="true"
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-aac="true"
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-qcp="true"
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += ril.subscription.types="NV,RUIM"
 PRODUCT_PROPERTY_OVERRIDES += ro.telephony.ril.v3="skippinpukcount,qcomdsds,signalstrength"
@@ -64,5 +65,5 @@ PRODUCT_PROPERTY_OVERRIDES += persist.data.ds_fmc_app.mode="false"
 PRODUCT_PROPERTY_OVERRIDES += persist.service.factory.enable="false"
 PRODUCT_PROPERTY_OVERRIDES += persist.radio.apm_sim_not_pwdn="true"
 # Debug
-PRODUCT_PROPERTY_OVERRIDES += debug.gralloc.map_fb_memory=true
-PRODUCT_PROPERTY_OVERRIDES += debug.hwc.fakevsync=true
+PRODUCT_PROPERTY_OVERRIDES += debug.gralloc.map_fb_memory="true"
+PRODUCT_PROPERTY_OVERRIDES += debug.hwc.fakevsync="true"
