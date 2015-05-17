c27c6b707f436ef6fab160ab3ff36b1dbddc620d
Caio99BR <caiooliveirafarias0@gmail.com>
2015-05-17 18:51:17 -0300

    Fix space

--- system_prop.mk
+++ system_prop.mk
@@ -42,11 +42,11 @@ PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-aac=true
 PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-qcp=true
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += rild.libpath=/system/lib/libril-qc-qmi-1.so
-PRODUCT_PROPERTY_OVERRIDES += rild.libargs=-d /dev/smd0
 PRODUCT_PROPERTY_OVERRIDES += ril.subscription.types=NV,RUIM
 PRODUCT_PROPERTY_OVERRIDES += ro.telephony.ril.v3=skippinpukcount,qcomdsds,signalstrength
 PRODUCT_PROPERTY_OVERRIDES += ro.telephony.call_ring.multiple=false
 PRODUCT_PROPERTY_OVERRIDES += ro.telephony.call_ring=false
+PRODUCT_PROPERTY_OVERRIDES += rild.libargs="-d /dev/smd0"
 # Debug
 PRODUCT_PROPERTY_OVERRIDES += debug.gralloc.map_fb_memory=true
 PRODUCT_PROPERTY_OVERRIDES += debug.hwc.fakevsync=true
