eec32bc7fa60e0f58799aeddd00fa50a239d0b29
Caio Oliveira <caiooliveirafarias0@gmail.com>
2015-07-23 22:20:37 -0300

    init.qcom.sh: New script
    The set of baseband is skipped in second boot, this reduce 5s
    The variant is now set here
    The Key Fix for L3 II Dual and L1 II is here
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -33,22 +33,76 @@ PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 export PATH
 
 # Set baseband based on modem
-v10=`strings /dev/block/mmcblk0p12 | grep -e "-V10.-" | head -1`
-v20=`strings /dev/block/mmcblk0p12 | grep -e "-V20.-" | head -1`
-setprop gsm.version.baseband ${v10}${v20}
+basebandcheck=`getprop gsm.version.baseband`
+case "$basebandcheck" in
+	"")
+	setprop gsm.version.baseband `strings /dev/block/mmcblk0p12 | grep -e "-V10.-" -e "-V20.-" | head -1`
+	;;
+esac
+
+# Get device based on baseband
+deviceset=`getprop gsm.version.baseband | grep -o -e "E410" -e "E411" -e "E415" -e "E420" -e "E425" -e "E430" -e "E431" -e "E435" | head -1`
+
+# Set Variant in build.prop
+mount -o rw,remount /system
+case "$deviceset" in
+	"E410")
+	busybox sed -i '/ro.product.model=L3 II/c\ro.product.model=E410 (L1 II Single)' system/build.prop
+	;;
+	"E411")
+	busybox sed -i '/ro.product.model=L3 II/c\ro.product.model=E411 (L1 II Single)' system/build.prop
+	;;
+	"E415")
+	busybox sed -i '/ro.product.model=L3 II/c\ro.product.model=E415 (L1 II Dual)' system/build.prop
+	;;
+	"E420")
+	busybox sed -i '/ro.product.model=L3 II/c\ro.product.model=E420 (L1 II Dual)' system/build.prop
+	;;
+	"E425")
+	busybox sed -i '/ro.product.model=L3 II/c\ro.product.model=E425 (L3 II Single)' system/build.prop
+	;;
+	"E430")
+	busybox sed -i '/ro.product.model=L3 II/c\ro.product.model=E430 (L3 II Single)' system/build.prop
+	;;
+	"E431")
+	busybox sed -i '/ro.product.model=L3 II/c\ro.product.model=E431 (L3 II Single)' system/build.prop
+	;;
+	"E435")
+	busybox sed -i '/ro.product.model=L3 II/c\ro.product.model=E435 (L3 II Dual)' system/build.prop
+	;;
+esac
+
+# Enable DualSim
+case "$deviceset" in
+	"E415" | "E420" | "E435")
+	setprop persist.radio.multisim.config dsds
+	setprop persist.multisim.config dsds
+	setprop ro.multi.rild true
+	stop ril-daemon
+	start ril-daemon
+	start ril-daemon1
+	;;
+esac
+
+# Change KeyLayouts
+case "$deviceset" in
+	"E410" | "E411" | "E415" | "E420")
+	cp system/usr/keylayout/touch_mcs8000.kl system/usr/keylayout/mms100s_ts.kl
+	cp system/usr/keylayout/vee3_keypad.kl system/usr/keylayout/v1_keypad.kl
+	;;
+	"E425" | "E430" | "E431")
+	busybox sed -i '/key 139   HOME              VIRTUAL/c\key 139   MENU              VIRTUAL' system/usr/keylayout/touch_mcs8000.kl
+	busybox sed -i '/key 172   MENU              VIRTUAL/c\key 172   HOME              VIRTUAL' system/usr/keylayout/touch_mcs8000.kl
+	;;
+	"E435")
+	busybox sed -i '/key 139   MENU              VIRTUAL/c\key 139   HOME              VIRTUAL' system/usr/keylayout/touch_mcs8000.kl
+	busybox sed -i '/key 172   HOME              VIRTUAL/c\key 172   MENU              VIRTUAL' system/usr/keylayout/touch_mcs8000.kl
+	;;
+esac
+mount -o ro,remount /system
 
 # Set essential configs
-roserialno=`getprop ro.serialno`
-romanufacturer=`getprop ro.product.manufacturer`
-romodel=`getprop ro.product.model`
-echo $roserialno > /sys/class/android_usb/android0/iSerial
-echo $romanufacturer > /sys/class/android_usb/android0/iManufacturer
-echo $romodel > /sys/class/android_usb/android0/iProduct
-echo $romanufacturer > /sys/class/android_usb/android0/f_rndis/manufacturer
-
-# Set DualSim based on baseband
-e425set=`getprop gsm.version.baseband | head -1 | grep -o "E425"`
-e430set=`getprop gsm.version.baseband | head -1 | grep -o "E430"`
-e431set=`getprop gsm.version.baseband | head -1 | grep -o "E431"`
-e435set=`getprop gsm.version.baseband | head -1 | grep -o "E435"`
-setprop persist.baseband.device ${e425set}${e430set}${e431set}${e435set}
+echo `getprop ro.serialno` > /sys/class/android_usb/android0/iSerial
+echo `getprop ro.product.manufacturer` > /sys/class/android_usb/android0/iManufacturer
+echo `getprop ro.product.manufacturer` > /sys/class/android_usb/android0/f_rndis/manufacturer
+echo `getprop ro.product.model` > /sys/class/android_usb/android0/iProduct
