1976d63f259de223dd9c4ae377e760585cc137a3
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-20 21:44:28 -0300

    init_vee3: Fix setting under bugged LolliPop
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- init/init_vee3.c
+++ init/init_vee3.c
@@ -36,34 +36,35 @@
 #include "util.h"
 
 void vendor_load_properties() {
-    char gversionbb[92];
-    char dversionbb[92];
+    //char gversionbb[92];
+    //char dversionbb[92];
     char o_gversionbb[92];
     char o_dversionbb[92];
-    FILE *fp;
+    //FILE *fp;
 
-    fp = popen("/system/xbin/printf $(/system/xbin/strings /dev/block/mmcblk0p12 | /system/bin/grep -e '-V10' -e '-V20')", "r");
-    fgets(gversionbb, sizeof(gversionbb), fp);
-    pclose(fp);
-    property_set("gsm.version.baseband", gversionbb);
+    //fp = popen("/system/xbin/printf $(/system/xbin/strings /dev/block/mmcblk0p12 | /system/bin/grep -e '-V10' -e '-V20')", "r");
+    //fgets(gversionbb, sizeof(gversionbb), fp);
+    //pclose(fp);
+    //property_set("gsm.version.baseband", gversionbb);
 
-    fp = popen("/system/xbin/printf $(/system/bin/getprop gsm.version.baseband | /system/bin/grep -o -e 'E425' -e 'E430' -e 'E431' -e 'E435')", "r");
-    fgets(dversionbb, sizeof(dversionbb), fp);
-    pclose(fp);
-    property_set("ro.product.device", dversionbb);
-    property_set("ro.product.model", dversionbb);
+    //fp = popen("/system/xbin/printf $(/system/bin/getprop gsm.version.baseband | /system/bin/grep -o -e 'E425' -e 'E430' -e 'E431' -e 'E435')", "r");
+    //fgets(dversionbb, sizeof(dversionbb), fp);
+    //pclose(fp);
+    //property_set("ro.product.device", dversionbb);
+    //property_set("ro.product.model", dversionbb);
 
-    if (strstr(dversionbb, "E435")) {
-        property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.multisim.config", "dsds");
-        property_set("ro.multi.rild", "true");
-    };
+    //if (strstr(dversionbb, "E435")) {
+        //property_set("persist.radio.multisim.config", "dsds");
+        //property_set("persist.multisim.config", "dsds");
+        //property_set("ro.multi.rild", "true");
+    //};
 
-    if (strlen(gversionbb) == 0) {
-        property_set("gsm.version.baseband", "V10");
+    //if (strlen(gversionbb) == 0) {
+        //property_set("gsm.version.baseband", "V10");
+        property_set("gsm.version.baseband", "No way to get - Unknown");
         property_set("ro.product.device", "vee3");
         property_set("ro.product.model", "vee3");
-    };
+    //};
 
     property_get("gsm.version.baseband", o_gversionbb);
     property_get("ro.product.device", o_dversionbb);
